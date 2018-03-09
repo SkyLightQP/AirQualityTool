424a3f8dae1db1395976ed48fd6f54a8f872aea8
SkyLightQP <combbm@naver.com>
2018-03-09 18:48:55 +0900

    NodeMCU 코드 업로드

---
+++ AirQuality/AirQuality.ino
@@ -0,0 +1,47 @@
+#include <Adafruit_Sensor.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+
+#define DHTPIN 5
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+String token = "token";
+int ms = 5000;
+
+HTTPClient http;
+
+void setup() {
+  Serial.begin(115200);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  dht.begin();
+}
+
+void loop(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  http.begin("http://localhost/arduino/" + (String)t + "/" + (String)h + "/" + token);
+  int httpCode = http.GET();  
+  if (httpCode > 0) {
+     String payload = http.getString();
+     Serial.println("Requested! " + payload);
+  }
+  delay(ms);
+}
